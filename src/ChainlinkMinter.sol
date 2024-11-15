aecacde3dab5164800410e77ad48da4743ea344e
cqlyj <luoyingjie0721@gmail.com>
2024-11-16 02:51:21 +0700

    feat: refactor ChainlinkMinter to integrate DummyContract and update upkeep logic

--- src/ChainlinkMinter.sol
+++ src/ChainlinkMinter.sol
@@ -1,49 +1,43 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.26;
 import {IAccountImplementation} from "../src/interfaces/IAccountImplementation.sol";
+import {AutomationCompatibleInterface} from "@chainlink/contracts/src/v0.8/automation/AutomationCompatible.sol";
+import {DummyContract} from "./DummyContract.sol";
 
-struct Log {
-    uint256 index; // Index of the log in the block
-    uint256 timestamp; // Timestamp of the block containing the log
-    bytes32 txHash; // Hash of the transaction containing the log
-    uint256 blockNumber; // Number of the block containing the log
-    bytes32 blockHash; // Hash of the block containing the log
-    address source; // Address of the contract that emitted the log
-    bytes32[] topics; // Indexed topics of the log
-    bytes data; // Data of the log
-}
+contract ChainlinkMinter is AutomationCompatibleInterface {
+    bool public s_accountCreated;
+    DummyContract public dummyContract;
 
-interface ILogAutomation {
-    function checkLog(
-        Log calldata log,
-        bytes memory checkData
-    ) external returns (bool upkeepNeeded, bytes memory performData);
-
-    function performUpkeep(bytes calldata performData) external;
-}
-
-contract ChainlinkMinter is ILogAutomation {
-    function checkLog(
-        Log calldata log,
-        bytes memory
-    ) external pure returns (bool upkeepNeeded, bytes memory performData) {
-        upkeepNeeded = true;
-        address user = bytes32ToAddress(log.topics[1]);
-        address campaign = bytes32ToAddress(log.topics[2]);
-        address account = bytes32ToAddress(log.topics[3]);
-        performData = abi.encode(user, campaign, account);
+    constructor(address _dummyContract) {
+        s_accountCreated = false;
+        dummyContract = DummyContract(_dummyContract);
     }
 
-    function performUpkeep(bytes calldata performData) external override {
-        (address user, address campaign, address account) = abi.decode(
-            performData,
-            (address, address, address)
-        );
+    function checkUpkeep(
+        bytes calldata /* checkData */
+    )
+        external
+        view
+        override
+        returns (bool upkeepNeeded, bytes memory /* performData */)
+    {
+        if (s_accountCreated) {
+            upkeepNeeded = true;
+        } else {
+            upkeepNeeded = false;
+        }
+    }
+
+    function performUpkeep(bytes calldata /*performData*/) external override {
+        address user = dummyContract.user();
+        address campaign = dummyContract.campaign();
+        address account = dummyContract.account();
         IAccountImplementation(account).initialize(user);
         IAccountImplementation(account).mintInteractionNFT(campaign);
+        s_accountCreated = false;
     }
 
-    function bytes32ToAddress(bytes32 _address) public pure returns (address) {
-        return address(uint160(uint256(_address)));
+    function setAccountCreated(bool created) external {
+        s_accountCreated = created;
     }
 }
