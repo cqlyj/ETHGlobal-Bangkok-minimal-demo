0f6c9cbbdf3d7284400b7c6558f480d0a8ac4198
cqlyj <luoyingjie0721@gmail.com>
2024-11-15 21:06:02 +0700

    feat: add initialize and mintInteractionNFT functions to IAccountImplementation interface

--- src/interfaces/IAccountImplementation.sol
+++ src/interfaces/IAccountImplementation.sol
@@ -3,4 +3,8 @@ pragma solidity 0.8.26;
 
 interface IAccountImplementation {
     function setApprovedMinter(address minter) external;
+
+    function initialize(address to) external;
+
+    function mintInteractionNFT(address campaign) external;
 }
