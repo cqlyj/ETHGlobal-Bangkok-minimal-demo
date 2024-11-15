dac60e9dacc60c39e20bab0f53d875408936e162
cqlyj <luoyingjie0721@gmail.com>
2024-11-15 21:06:18 +0700

    feat: modify DeployChainlinkMinter script to instantiate ChainlinkMinter without ACCOUNT_IMPLEMENTATION_ADDRESS

--- script/DeployChainlinkMinter.s.sol
+++ script/DeployChainlinkMinter.s.sol
@@ -5,13 +5,9 @@ import {Script} from "forge-std/Script.sol";
 import {ChainlinkMinter} from "src/ChainlinkMinter.sol";
 
 contract DeployChainlinkMinter is Script {
-    address public constant ACCOUNT_IMPLEMENTATION_ADDRESS = 0xa6eEE68A4324F8DA953dA58DbC9C05781EbB04F9;
-
     function run() external returns (ChainlinkMinter) {
         vm.startBroadcast();
-        ChainlinkMinter chainlinkMinter = new ChainlinkMinter(
-            ACCOUNT_IMPLEMENTATION_ADDRESS
-        );
+        ChainlinkMinter chainlinkMinter = new ChainlinkMinter();
         vm.stopBroadcast();
         return (chainlinkMinter);
     }
