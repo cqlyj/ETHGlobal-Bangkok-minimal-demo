d089e79a957368a924cd29dfc15240b08dd66dab
cqlyj <luoyingjie0721@gmail.com>
2024-11-15 19:43:50 +0700

    feat: add deployment script for AccountImplementation contract

---
+++ script/DeployAccountImplementation.s.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.26;
+
+import {Script} from "forge-std/Script.sol";
+import {AccountImplementation} from "src/AccountImplementation.sol";
+
+contract DeployAccountImplementation is Script {
+    function run() external returns (AccountImplementation) {
+        vm.startBroadcast();
+        AccountImplementation accountImplementation = new AccountImplementation();
+        vm.stopBroadcast();
+        return (accountImplementation);
+    }
+}
